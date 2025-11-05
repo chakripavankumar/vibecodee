c90df34ae539f71e89ad09407c32cded7f17d0f1
Pavan Kumar <siriyalapavankumar@gmail.com>
2025-11-05 19:31:10 +0530

    configured & authsetup

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,12 @@
+<<<<<<< Updated upstream
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+=======
+import { PrismaClient } from "./generated/prisma/client";
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+>>>>>>> Stashed changes
 
 export const db = globalForPrisma.prisma || new PrismaClient();
 
